b54b40f53a131e4e95695fd62a287a9f5945e3f5
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2025-08-26 10:32:53 +0530

    Create 844_Backspace String Compare.py

---
+++ Easy/844_Backspace String Compare.py
@@ -0,0 +1,12 @@
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        def build(string):
+            stack = []
+            for char in string:
+                if char != "#":
+                    stack.append(char)
+                elif stack:
+                    stack.pop()
+            return "".join(stack)
+        return build(s) == build(t)
+
